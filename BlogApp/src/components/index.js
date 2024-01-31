4446224154e0d5ae924529f044768ade11a1f59e
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-01-31 20:09:02 +0530

    main.jsx completed

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -6,9 +6,9 @@ import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE"
 import Signup from "./Signup";
 import Login from "./Login"
-import Post from "./Post"
 import PostForm from "./post-form/PostForm"
 import PostCard from "./PostCard"
+import Authlayout from "./AuthLayout"
 
 export {
     Header, 
@@ -19,8 +19,8 @@ export {
     RTE,
     Signup,
     Login,
-    Post,
     PostForm,
-    PostCard
+    PostCard,
+    Authlayout
 
 }
